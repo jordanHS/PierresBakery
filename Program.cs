074803f9680fa3b7cd70e0fe6b08c34a7a07c6b8
Jordan Safford <jordansafford@gmail.com>
2020-01-17 21:14:27 -0800

    remove excess code from Program

--- Program.cs
+++ Program.cs
@@ -13,11 +13,9 @@ namespace PierresBakery
             Bread myBread = new Bread(0);
             Console.WriteLine("How many loaves would you like? (Please use numbers only)");
             myBread.BreadQuantity = int.Parse(Console.ReadLine());
-            int breadTotal = myBread.BreadQuantity * myBread.Cost;
             Pastries myPastries = new Pastries(0);
             Console.WriteLine("How many pastries would you like?");
             myPastries.PastryQuantity=int.Parse(Console.ReadLine());
-            int pastryTotal = myPastries.PastryQuantity * myPastries.Cost;
             Console.WriteLine("You bought {0} loaves of bread and {1} pastries.", myBread.BreadQuantity, myPastries.PastryQuantity);
             int orderTotal = myBread.GetTotal() + myPastries.GetTotal();
             string stringTotal = orderTotal.ToString();            
